9d5a015d64a33024cbd59a8ede4069ab1011bfc8
Kent Odde <kentodde89@gmail.com>
2020-09-05 16:31:25 +0200

    changed simpleRSA

--- SimpleRSA/main.cpp
+++ SimpleRSA/main.cpp
@@ -3,11 +3,9 @@
 #include <cstdlib>
 #include <ctime>
 #include <math.h> 
-#include<gmp.h>
 #include <unistd.h>
 #include <algorithm>
 uintmax_t generateRandomNumber(uintmax_t min, uintmax_t max);
-
 
 
 uintmax_t power(uintmax_t x, uintmax_t y, uintmax_t p)
@@ -81,18 +79,6 @@ bool isPrime(uintmax_t n, uintmax_t k)
 		if(millerTest(n, d) == false) return false;
 	}
 	return true;
-
-	/*uintmax_t i = 5;
-
-	while(i * i <= n)
-	{
-		if(n % i == 0 || n % (i+2) == 0)
-		{
-			return false;
-		}
-		i += 6;
-	}
-	return true;*/
 }
 
 uintmax_t generateRandomNumber(uintmax_t min, uintmax_t max)
@@ -128,21 +114,16 @@ intmax_t modInv(uintmax_t a, uintmax_t m)
   
     while (a > 1) 
     { 
-        // q is quotient 
         intmax_t q = a / m; 
         intmax_t t = m; 
   
-        // m is remainder now, process same as 
-        // Euclid's algo 
         m = a % m, a = t; 
         t = y; 
   
-        // Update y and x 
         y = x - q * y; 
         x = t; 
     } 
-  
-    // Make x positive 
+   
     if (x < 0) 
        x += m0; 
   
@@ -165,13 +146,13 @@ bool generateKeys(uintmax_t& N, uintmax_t& e, uintmax_t & d)
 
 	while(true)
 	{
-		p = 11;//generateRandomPrime(k/2);
+		p = generateRandomPrime(k/2);
 		if(p % e != 1) break;
 	}
 
 	while(true)
 	{
-		q = 7;//generateRandomPrime(k - k/2);
+		q = generateRandomPrime(k - k/2);
 		if(q % e != 1) break;
 	}
 
@@ -206,45 +187,6 @@ uintmax_t decrypt(uintmax_t m, uintmax_t n, uintmax_t d)
 	return power(m, d, n);
 }
 
-uintmax_t stringToInt(std::string s)
-{
-	//Replace the ascii value of the string with the character. Add a 0 in front if it has less than 3 digits. 
-	//Read in a 1 first, so that the 0 won't be deleted. 
-	s.insert(0, "1");
-	for(int i = s.size() - 1; i > 0; i--)
-	{
-		std::string replacementString = std::to_string((int)s.at(i));
-		std::cout << "ReplacementString " << replacementString << " Size: " << replacementString.size() << "\n";
-		while(replacementString.size() != 3)
-		{
-			std::cout << "NOT 3!!\n";
-			replacementString.insert(0, "0");
-		} 
-		std::cout << "ReplacementString " << replacementString << " Size: " << replacementString.size() << "\n";
-
-		s.replace(i, 1, replacementString);
-	}
-	return std::stoi(s.c_str());
-}
-
-std::string intToString(uintmax_t i)
-{
-	std::string s = std::to_string(i);
-	s.erase(0, 1);
-
-	for(int i = s.size()-3; i >= 0; i -= 3)
-	{
-		std::string substr = s.substr(i, 3);
-		std::cout <<"substr: " << substr << "\n";
-		int asc = std::stoi(substr.c_str());
-		char c = asc;
-		std::cout << "char " << c << "\n";
-		
-		s.replace(i, 3, std::string(1, c));
-	}
-
-	return s;
-}
 
 int main()
 {
@@ -257,19 +199,13 @@ int main()
 	std::cout << "e: " << e << "\n";
 	std::cout << "d: " << d << "\n";
 
-	const char* bytes = "d";
-
-	std::string message = "D";
-	uintmax_t intMessage = stringToInt(message);
+	uintmax_t intMessage = 7;
 	
-	std::cout << "Original message: " << message << "\n";
-	std::cout << "Converted to int is: " << intMessage << "\n";
 	uintmax_t encrypted = encrypt(intMessage, N, e);
 	std::cout << intMessage << " encrypted with Public is: " << encrypted << "\n";
 
 	uintmax_t decrypted = decrypt(encrypted, N, d);
 	std::cout << encrypted << " decrypted with Private is: " << decrypted << "\n";
-	std::cout << "Decrypted to String is: \n" << intToString(decrypted) << "\n";
 
 	uintmax_t encrypted2 = encrypt(intMessage, N, d);
 	std::cout << intMessage << " encrypted with Private is: " << encrypted2 << "\n";
