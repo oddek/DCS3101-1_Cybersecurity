921739c5c9e268d63dcd6c6e4303c3c0c718c04a
Kent Odde <kentodde89@gmail.com>
2020-09-18 18:53:45 +0200

    Got decryption to work in some cases

--- AES/main.cpp
+++ AES/main.cpp
@@ -13,6 +13,46 @@ std::vector<int> RCON_BOX =
 	{0x00, 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1b, 0x36} 
 };
 
+std::vector<std::vector<int>> L_TABLE =
+{
+	{0x00, 0x00, 0x19, 0x01, 0x32, 0x02, 0x1A, 0xC6, 0x4B, 0xC7, 0x1B, 0x68, 0x33, 0xEE, 0xDF, 0x03},
+	{0x64, 0x04, 0xE0, 0x0E, 0x34, 0x8D, 0x81, 0xEF, 0x4C, 0x71, 0x08, 0xC8, 0xF8, 0x69, 0x1C, 0xC1}, 
+	{0x7D, 0xC2, 0x1D, 0xB5, 0xF9, 0xB9, 0x27, 0x6A, 0x4D, 0xE4, 0xA6, 0x72, 0x9A, 0xC9, 0x09, 0x78}, 
+	{0x65, 0x2F, 0x8A, 0x05, 0x21, 0x0F, 0xE1, 0x24, 0x12, 0xF0, 0x82, 0x45, 0x35, 0x93, 0xDA, 0x8E}, 
+	{0x96, 0x8F, 0xDB, 0xBD, 0x36, 0xD0, 0xCE, 0x94, 0x13, 0x5C, 0xD2, 0xF1, 0x40, 0x46, 0x83, 0x38},
+	{0x66, 0xDD, 0xFD, 0x30, 0xBF, 0x06, 0x8B, 0x62, 0xB3, 0x25, 0xE2, 0x98, 0x22, 0x88, 0x91, 0x10}, 
+	{0x7E, 0x6E, 0x48, 0xC3, 0xA3, 0xB6, 0x1E, 0x42, 0x3A, 0x6B, 0x28, 0x54, 0xFA, 0x85, 0x3D, 0xBA}, 
+	{0x2B, 0x79, 0x0A, 0x15, 0x9B, 0x9F, 0x5E, 0xCA, 0x4E, 0xD4, 0xAC, 0xE5, 0xF3, 0x73, 0xA7, 0x57}, 
+	{0xAF, 0x58, 0xA8, 0x50, 0xF4, 0xEA, 0xD6, 0x74, 0x4F, 0xAE, 0xE9, 0xD5, 0xE7, 0xE6, 0xAD, 0xE8}, 
+	{0x2C, 0xD7, 0x75, 0x7A, 0xEB, 0x16, 0x0B, 0xF5, 0x59, 0xCB, 0x5F, 0xB0, 0x9C, 0xA9, 0x51, 0xA0}, 
+	{0x7F, 0x0C, 0xF6, 0x6F, 0x17, 0xC4, 0x49, 0xEC, 0xD8, 0x43, 0x1F, 0x2D, 0xA4, 0x76, 0x7B, 0xB7}, 
+	{0xCC, 0xBB, 0x3E, 0x5A, 0xFB, 0x60, 0xB1, 0x86, 0x3B, 0x52, 0xA1, 0x6C, 0xAA, 0x55, 0x29, 0x9D}, 
+	{0x97, 0xB2, 0x87, 0x90, 0x61, 0xBE, 0xDC, 0xFC, 0xBC, 0x95, 0xCF, 0xCD, 0x37, 0x3F, 0x5B, 0xD1},
+	{0x53, 0x39, 0x84, 0x3C, 0x41, 0xA2, 0x6D, 0x47, 0x14, 0x2A, 0x9E, 0x5D, 0x56, 0xF2, 0xD3, 0xAB},
+	{0x44, 0x11, 0x92, 0xD9, 0x23, 0x20, 0x2E, 0x89, 0xB4, 0x7C, 0xB8, 0x26, 0x77, 0x99, 0xE3, 0xA5}, 
+	{0x67, 0x4A, 0xED, 0xDE, 0xC5, 0x31, 0xFE, 0x18, 0x0D, 0x63, 0x8C, 0x80, 0xC0, 0xF7, 0x70, 0x07}
+};
+
+std::vector<std::vector<int>> E_TABLE = 
+{
+	{0x01, 0x03, 0x05, 0x0F, 0x11, 0x33, 0x55, 0xFF, 0x1A, 0x2E, 0x72, 0x96, 0xA1, 0xF8, 0x13, 0x35}, 
+	{0x5F, 0xE1, 0x38, 0x48, 0xD8, 0x73, 0x95, 0xA4, 0xF7, 0x02, 0x06, 0x0A, 0x1E, 0x22, 0x66, 0xAA}, 
+	{0xE5, 0x34, 0x5C, 0xE4, 0x37, 0x59, 0xEB, 0x26, 0x6A, 0xBE, 0xD9, 0x70, 0x90, 0xAB, 0xE6, 0x31}, 
+	{0x53, 0xF5, 0x04, 0x0C, 0x14, 0x3C, 0x44, 0xCC, 0x4F, 0xD1, 0x68, 0xB8, 0xD3, 0x6E, 0xB2, 0xCD}, 
+	{0x4C, 0xD4, 0x67, 0xA9, 0xE0, 0x3B, 0x4D, 0xD7, 0x62, 0xA6, 0xF1, 0x08, 0x18, 0x28, 0x78, 0x88}, 
+	{0x83, 0x9E, 0xB9, 0xD0, 0x6B, 0xBD, 0xDC, 0x7F, 0x81, 0x98, 0xB3, 0xCE, 0x49, 0xDB, 0x76, 0x9A}, 
+	{0xB5, 0xC4, 0x57, 0xF9, 0x10, 0x30, 0x50, 0xF0, 0x0B, 0x1D, 0x27, 0x69, 0xBB, 0xD6, 0x61, 0xA3}, 
+	{0xFE, 0x19, 0x2B, 0x7D, 0x87, 0x92, 0xAD, 0xEC, 0x2F, 0x71, 0x93, 0xAE, 0xE9, 0x20, 0x60, 0xA0}, 
+	{0xFB, 0x16, 0x3A, 0x4E, 0xD2, 0x6D, 0xB7, 0xC2, 0x5D, 0xE7, 0x32, 0x56, 0xFA, 0x15, 0x3F, 0x41}, 
+	{0xC3, 0x5E, 0xE2, 0x3D, 0x47, 0xC9, 0x40, 0xC0, 0x5B, 0xED, 0x2C, 0x74, 0x9C, 0xBF, 0xDA, 0x75}, 
+	{0x9F, 0xBA, 0xD5, 0x64, 0xAC, 0xEF, 0x2A, 0x7E, 0x82, 0x9D, 0xBC, 0xDF, 0x7A, 0x8E, 0x89, 0x80}, 
+	{0x9B, 0xB6, 0xC1, 0x58, 0xE8, 0x23, 0x65, 0xAF, 0xEA, 0x25, 0x6F, 0xB1, 0xC8, 0x43, 0xC5, 0x54}, 
+	{0xFC, 0x1F, 0x21, 0x63, 0xA5, 0xF4, 0x07, 0x09, 0x1B, 0x2D, 0x77, 0x99, 0xB0, 0xCB, 0x46, 0xCA}, 
+	{0x45, 0xCF, 0x4A, 0xDE, 0x79, 0x8B, 0x86, 0x91, 0xA8, 0xE3, 0x3E, 0x42, 0xC6, 0x51, 0xF3, 0x0E}, 
+	{0x12, 0x36, 0x5A, 0xEE, 0x29, 0x7B, 0x8D, 0x8C, 0x8F, 0x8A, 0x85, 0x94, 0xA7, 0xF2, 0x0D, 0x17}, 
+	{0x39, 0x4B, 0xDD, 0x7C, 0x84, 0x97, 0xA2, 0xFD, 0x1C, 0x24, 0x6C, 0xB4, 0xC7, 0x52, 0xF6, 0x01}, 
+};
+
 std::vector<std::vector<int>> S_BOX = 
 {
  	{0x63 ,0x7c ,0x77 ,0x7b ,0xf2 ,0x6b ,0x6f ,0xc5 ,0x30 ,0x01 ,0x67 ,0x2b ,0xfe ,0xd7 ,0xab ,0x76},
@@ -61,6 +101,14 @@ std::vector<std::vector<int>> MIX_COL_MATRIX =
 	{3, 1, 1, 2}
 };
 
+std::vector<std::vector<int>> INV_MIX_COL_MATRIX = 
+{
+	{0x0E, 0x0B, 0x0D, 0x09},
+	{0x09, 0x0E, 0x0B, 0x0D},
+	{0x0D, 0x09, 0x0E, 0x0B},
+	{0x0B, 0x0D, 0x09, 0x0E}
+};
+
 
 //Operations:
 void addRoundKey(std::vector<std::vector<std::bitset<8>>>& grid, std::vector<std::vector<std::bitset<8>>>& key);
@@ -69,9 +117,9 @@ void shiftGrid(std::vector<std::vector<std::bitset<8>>>& grid);
 void mixColumns(std::vector<std::vector<std::bitset<8>>>& grid);
 
 //Inverse Operations:
-
 void invSubstitute(std::vector<std::vector<std::bitset<8>>>& grid);
-void InvShiftGrid(std::vector<std::vector<std::bitset<8>>>& grid);
+void invShiftGrid(std::vector<std::vector<std::bitset<8>>>& grid);
+void invMixColumns(std::vector<std::vector<std::bitset<8>>>& grid);
 
 //Key
 std::vector<std::vector<std::bitset<8>>> generateKey();
@@ -90,19 +138,49 @@ void printExpandedKey(std::vector<std::vector<std::vector<std::bitset<8>>>>& key
 std::vector<std::vector<std::bitset<8>>> generateGridFromPlainText(std::string message);
 std::vector<std::vector<std::bitset<8>>> generateGridFromHexString(std::string s);
 std::string gridToHexString(std::vector<std::vector<std::bitset<8>>> grid);
+std::string hexStringToPlaintext(std::string s);
 
 void runTests();
 
 //Interface
 std::string encrypt128BitMessage(std::vector<std::vector<std::bitset<8>>> grid, std::vector<std::vector<std::bitset<8>>>& key);
-
-
-
-
-
+std::string decrypt128BitMessage(std::vector<std::vector<std::bitset<8>>> grid, std::vector<std::vector<std::bitset<8>>>& key);
 
 int main()
 {
+
+
+	/*
+	std::string key = "2b28ab097eaef7cf15d2154f16a6883c";
+	std::string enc = "5a9bece90c6ababf667f2d07416f45a1";
+	auto g = generateGridFromHexString(enc);
+	auto k = generateGridFromHexString(key);
+	std::cout << "Key: \n\t" << key << "\n";
+	std::cout << "Trying to decrypt: \n\t" << enc << "\n";
+
+	std::string dec = decrypt128BitMessage(g, k);
+
+	std::string decPlain = hexStringToPlaintext(dec);
+
+	std::cout << "DecryptedMessage: \n\t" << dec << "\n";
+	std::cout << "DecryptedPlaintext: \n\t" << decPlain << "\n";
+
+
+	g = generateGridFromHexString(dec);
+	std::string encryptedAgain = encrypt128BitMessage(g, k);
+
+	std::cout << "EncryptedBack: \n\t" << encryptedAgain << "\n";
+
+*/
+	/*std::string p = "046681E5046681E5046681E5046681E5";
+	auto g = generateGridFromHexString(p);
+	printGrid(g);
+	invMixColumns(g);
+	printGrid(g);
+*/
+
+
+
 	bool test = true;
 	bool randomKey = false;
 
@@ -112,7 +190,7 @@ int main()
 		return 0;
 	}
 
-	std::string plaintext = 	"katthundfisklakskentoddeheterjeg";
+	std::string plaintext = 	"katthundfisklaks";
 	std::string keyString = 	"FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF";
 	auto key = generateGridFromHexString(keyString);
 
@@ -126,9 +204,30 @@ int main()
 	std::cout << "Key: \n\t" << keyString << "\n";
 
 	std::string encrypted = encrypt128BitMessage(grid, key);
-	std::cout << "EncryptedMessage: \n\t" << encrypted;
+	std::cout << "EncryptedMessage: \n\t" << encrypted << "\n";
+
+	auto grid2 = generateGridFromHexString(encrypted);
+
+	std::string decrypted = decrypt128BitMessage(grid2, key);
+	std::cout << "DecryptedMessage: \n\t" << decrypted << "\n";
+
+	std::string decPlain = hexStringToPlaintext(decrypted);
+	std::cout << "DecryptedPlainText: \n\t" << decPlain << "\n";
+
 	return 0;
 }
+
+std::string hexStringToPlaintext(std::string s)
+{
+	std::string plain = "";
+	for(int i = 0; i < s.size(); i+=2)
+	{
+		char temp = stoul(s.substr(i, 2), 0, 16);
+		plain += temp;
+	}
+	return plain;
+}
+
 
 std::vector<std::vector<std::bitset<8>>> generateGridFromHexString(std::string s)
 {
@@ -144,6 +243,29 @@ std::vector<std::vector<std::bitset<8>>> generateGridFromHexString(std::string s
 		}
 	}
 	return grid;
+}
+
+std::string decrypt128BitMessage(std::vector<std::vector<std::bitset<8>>> grid, std::vector<std::vector<std::bitset<8>>>& key)
+{
+	auto expandedKey = generateExpandedKey(key);
+	//printExpandedKey(expandedKey);
+	//Initial Round:
+	addRoundKey(grid, expandedKey.at(expandedKey.size() - 1));
+
+	//Rounds 1-10
+	for(int i = expandedKey.size() - 2; i > 0; i--)
+	{
+		invShiftGrid(grid);
+		invSubstitute(grid);
+		addRoundKey(grid, expandedKey.at(i));
+		invMixColumns(grid);
+	}
+
+	invShiftGrid(grid);
+	invSubstitute(grid);
+	addRoundKey(grid, expandedKey.at(0));
+	//printGrid(grid);
+	return gridToHexString(grid);
 }
 
 
@@ -306,13 +428,62 @@ void mixColumns(std::vector<std::vector<std::bitset<8>>>& grid)
 	grid = newGrid;
 }
 
+std::bitset<8> L(std::bitset<8> byte)
+{
+	std::bitset<8> divider(0x0F);
+	unsigned col = std::bitset<4>((byte & divider).to_ulong()).to_ulong();
+	unsigned row = std::bitset<4>(((byte >> 4) & divider).to_ulong()).to_ulong();
+
+	return std::bitset<8>(L_TABLE.at(row).at(col));	
+}
+
+
+std::bitset<8> E(std::bitset<8> byte)
+{
+	std::bitset<8> divider(0x0F);
+	unsigned col = std::bitset<4>((byte & divider).to_ulong()).to_ulong();
+	unsigned row = std::bitset<4>(((byte >> 4) & divider).to_ulong()).to_ulong();
+
+	return std::bitset<8>(E_TABLE.at(row).at(col));	
+}
+
+
+void invMixColumns(std::vector<std::vector<std::bitset<8>>>& grid)
+{
+	std::vector<std::vector<std::bitset<8>>> newGrid(4, std::vector<std::bitset<8>>(4));
+
+	for(int row = 0; row < N; row++)
+	{
+		for(int col = 0; col < N; col++)
+		{
+			std::vector<std::bitset<8>> c;
+
+			for(int tempRow = 0; tempRow < N; tempRow++)
+			{
+				unsigned long t1 = L(grid.at(tempRow).at(col)).to_ulong();
+				unsigned long t2 = L(INV_MIX_COL_MATRIX.at(row).at(tempRow)).to_ulong();
+				unsigned long t3 = t1 + t2;
+				if(t3 > 0xFF) t3 -= 0xFF;
+
+				c.push_back(E(std::bitset<8>(t3)));
+				//c.push_back(E(L(grid.at(tempRow).at(col)) ^ L(INV_MIX_COL_MATRIX.at(row).at(tempRow))));
+			}
+			std::bitset<8> final;
+			for(auto i : c) final ^= i;
+			newGrid.at(row).at(col) = final;
+
+		}
+	}
+	grid = newGrid;
+}
+
+
 
 std::bitset<8> ffMultiply(std::bitset<8> a, std::bitset<8> b)
 {
 	//std::cout << "Multi " << std::hex << a.to_ulong() << ", with " << std::hex << b.to_ulong() << "\n";
 	if(b.to_ulong() == 0x01)
 	{
-		
 		return a;
 	} 
  	std::bitset<8> magicByte(std::string("00011011"));
@@ -373,7 +544,7 @@ void shiftGrid(std::vector<std::vector<std::bitset<8>>>& grid)
 	}
 }
 
-void InvShiftGrid(std::vector<std::vector<std::bitset<8>>>& grid)
+void invShiftGrid(std::vector<std::vector<std::bitset<8>>>& grid)
 {
 	for(int i = 1; i < N; i++)
 	{
@@ -431,6 +602,7 @@ void runTests()
 
 	std::cout << "Running tests:\n";
 
+//TEST1
 	std::string plaintext = 	"6bc1bee22e409f96e93d7e117393172a";
 	std::string keyString = 	"2b7e151628aed2a6abf7158809cf4f3c";
 	std::string cipherText = 	"3ad77bb40d7a3660a89ecaf32466ef97";
@@ -439,7 +611,8 @@ void runTests()
 	auto cipher = generateGridFromHexString(cipherText);
 
 	std::string encrypted = encrypt128BitMessage(grid, key);
-	std::cout << "\tTest 1: ";
+	std::cout << "\tTest 1: \n";
+	std::cout << "\t\tEncryption: ";
 	if(cipherText.compare(encrypted) == 0)
 	{
 		std::cout << "Passed\n";
@@ -448,7 +621,25 @@ void runTests()
 	{
 		std::cout << "Failed\n";
 	}
+	//std::cout << encrypted << "\n";
+	//std::cout << cipherText << "\n";
+	grid = generateGridFromHexString(encrypted);
+	std::string decrypted = decrypt128BitMessage(grid, key);
 
+	//std::cout << plaintext << "\n";
+	//std::cout << decrypted << "\n";
+
+	std::cout << "\t\tDecryption: ";
+	if(decrypted.compare(plaintext) == 0)
+	{
+		std::cout << "Passed\n";
+	}
+	else
+	{
+		std::cout << "Failed\n";
+	}
+
+//TEST2
 	plaintext = 	"ae2d8a571e03ac9c9eb76fac45af8e51";
 	keyString = 	"2b7e151628aed2a6abf7158809cf4f3c";
 	cipherText = 	"f5d3d58503b9699de785895a96fdbaaf";
@@ -458,7 +649,8 @@ void runTests()
 
 	encrypted = encrypt128BitMessage(grid, key);
 
-	std::cout << "\tTest 2: ";
+	std::cout << "\tTest 2: \n";
+	std::cout << "\t\tEncryption: ";
 	if(cipherText.compare(encrypted) == 0)
 	{
 		std::cout << "Passed\n";
@@ -468,6 +660,25 @@ void runTests()
 		std::cout << "Failed\n";
 	}
 
+	//std::cout << encrypted << "\n";
+	//std::cout << cipherText << "\n";
+	grid = generateGridFromHexString(encrypted);
+	decrypted = decrypt128BitMessage(grid, key);
+
+	//std::cout << plaintext << "\n";
+	//std::cout << decrypted << "\n";
+
+	std::cout << "\t\tDecryption: ";
+	if(decrypted.compare(plaintext) == 0)
+	{
+		std::cout << "Passed\n";
+	}
+	else
+	{
+		std::cout << "Failed\n";
+	}
+
+//TEST3
 	plaintext = 	"30c81c46a35ce411e5fbc1191a0a52ef";
 	keyString = 	"2b7e151628aed2a6abf7158809cf4f3c";
 	cipherText = 	"43b1cd7f598ece23881b00e3ed030688";
@@ -477,7 +688,9 @@ void runTests()
 
 	encrypted = encrypt128BitMessage(grid, key);
 
-	std::cout << "\tTest 2: ";
+	std::cout << "\tTest 2: \n";
+	std::cout << "\t\tEncryption: ";
+
 	if(cipherText.compare(encrypted) == 0)
 	{
 		std::cout << "Passed\n";
@@ -487,6 +700,21 @@ void runTests()
 		std::cout << "Failed\n";
 	}
 
+	grid = generateGridFromHexString(encrypted);
+	decrypted = decrypt128BitMessage(grid, key);
+
+	std::cout << "\t\tDecryption: ";
+	if(decrypted.compare(plaintext) == 0)
+	{
+		std::cout << "Passed\n";
+	}
+	else
+	{
+		std::cout << "Failed\n";
+	}
+
+
+//TEST4
 	plaintext = 	"f69f2445df4f9b17ad2b417be66c3710";
 	keyString = 	"2b7e151628aed2a6abf7158809cf4f3c";
 	cipherText = 	"7b0c785e27e8ad3f8223207104725dd4";
@@ -496,8 +724,27 @@ void runTests()
 
 	encrypted = encrypt128BitMessage(grid, key);
 
-	std::cout << "\tTest 4: ";
+	std::cout << "\tTest 4: \n";
+	std::cout << "\t\tEncryption: ";
+
 	if(cipherText.compare(encrypted) == 0)
+	{
+		std::cout << "Passed\n";
+	}
+	else
+	{
+		std::cout << "Failed\n";
+	}
+	//std::cout << encrypted << "\n";
+	//std::cout << cipherText << "\n";
+	grid = generateGridFromHexString(encrypted);
+	decrypted = decrypt128BitMessage(grid, key);
+
+	//std::cout << plaintext << "\n";
+	//std::cout << decrypted << "\n";
+
+	std::cout << "\t\tDecryption: ";
+	if(decrypted.compare(plaintext) == 0)
 	{
 		std::cout << "Passed\n";
 	}
