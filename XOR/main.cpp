765f480535bcd1ea2c05fd3ceb94ac37d1e9a6be
Kent Odde <kentodde89@gmail.com>
2020-10-03 13:00:54 +0200

    added XOR helper and Oblig 2

--- XOR/main.cpp
+++ XOR/main.cpp
@@ -7,10 +7,18 @@
 
 int main()
 {
-	std::bitset<8> a("11111111");
-	std::bitset<8> b("01010101");
 
-	std::bitset<8> c = (a ^= b);
 
-	std::cout << c;
+
+	std::bitset<16> a(0x0B1A);
+	std::bitset<16> b(0x4f58);
+
+	std::bitset<16> c = (a ^ b);
+
+	std::cout << a << "\n";
+	std::cout << b << "\n";
+
+	std::cout << std::hex << c.to_ulong() << "\n";
+
+	std::cout << c << "\n";
 }
